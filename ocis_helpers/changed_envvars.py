bf3caf34d87da413f75e75483063eb91c613fd3d
Martin Mattel <github@diemattels.at>
2026-08-18 14:22:59 +0200

    fix: [master] code update and parametrisation

--- ocis_helpers/changed_envvars.py
+++ ocis_helpers/changed_envvars.py
@@ -30,17 +30,17 @@ from urllib.request import urlopen
 # 'versionOld' is the base version to compare from
 # 'versionNew' is the target version to compare to
 # branches are written the follwoing: 'master' or 'x,y' such as '8.1'
-versionOld: str    = '8.0'
-versionNew: str    = '8.1'
+versionOld: str    = '8.2'
+versionNew: str    = 'master' #'8.3'
 
 # CHANGE according your needs (for printing, to_version also to identify all other findings that should be excluded)
-from_version: str  = '8.0.0'
-to_version: str    = '8.1.0'
+from_version: str  = '8.2.0'
+to_version: str    = '8.3.0'
 
 # CHANGE according your needs (for file names to be created)
 # this will create files such as 8.0.0-8.1.0-added, 8.0.0-8.1.0-removed etc.
 # this must match which versions you compare.
-nameComponent: str = '8.0.0-8.1.0'
+nameComponent: str = '8.2.0-8.3.0'
 
 # ADD elements of version that have been published so they get EXCLUDED from gathering.
 # from version 7 onwards we dont use patch versions anymore - they should be present.
@@ -49,7 +49,7 @@ nameComponent: str = '8.0.0-8.1.0'
 defaultExcludePattern: list[str] = ['pre5.0', '5.0',
 									'6.0', '6.0.0', '6.0.1', '6.1.0', '6.7',
 									'7.0', '7.0.0', '7.1.0', '7.2.0', '7.3.0',
-									'8.0.0']
+									'8.0.0', '8.1.0', '8.2.0']
 
 # IMPORTANT: there may be additional unexpected versions that may appear from backporting in ocis which
 # must be excluded too. if found, they are printed to the console for furter investigation.
@@ -82,10 +82,12 @@ def merge_exclude_lists(defaultExcludePattern, extraExcludePattern) -> list[str]
 	return merged_array
 
 def get_cli_dryrun_param() -> bool:
+	# exit if there is no parameter added
+	if (len(sys.argv) -1) == 0:
+		return False
+	
 	# we only allow one cli parameter which is -h (help) or -d (dryrun)
 	param_1 = sys.argv[1]
-	if len(param_1) <= 1:
-		return False
 	match param_1:
 		case '-d':
 			return True
